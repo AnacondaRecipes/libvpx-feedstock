58d991ecf6cece9315861fc6174955448c68f386
Anthony DiPietro <adipietro@anaconda.com>
2021-12-10 16:02:54 -0500

    Pull over recipe updates from aggregate

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,21 +2,30 @@
 
 set -ex
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+
+if [[ $(uname) == MSYS* ]]; then
+  if [[ ${ARCH} == 32 ]]; then
+    HOST_BUILD="--host=i686-w64-mingw32 --build=i686-w64-mingw32"
+  else
+    HOST_BUILD="--host=x86_64-w64-mingw32 --build=x86_64-w64-mingw32"
+  fi
+  PREFIX=${PREFIX}/Library/mingw-w64
+fi
+
 if [[ ${target_platform} == linux-* ]]; then
   LDFLAGS="$LDFLAGS -pthread"
 fi
 
-if [[ ${target_platform} == osx-arm64 ]]; then
-  TARGET="--target=arm64-darwin20-gcc"
-  export CROSS=arm64-apple-darwin20.0.0-
-  # -fembed-bitcode conflicts with a conda-forge LD option (-dead_strip_dylibs)
-  sed -i.bak "/check_add_ldflags -fembed-bitcode/d" build/make/configure.sh
+CPU_DETECT=""
+if [[ ${target_platform} == osx-* ]]; then
+  CPU_DETECT="${CPU_DETECT} --disable-avx512 --disable-runtime-cpu-detect"
 else
-  CPU_DETECT="--enable-runtime-cpu-detect"
+  CPU_DETECT="${CPU_DETECT} --enable-runtime-cpu-detect"
 fi
 
-
-./configure --prefix=${PREFIX} ${TARGET} \
+./configure --prefix=${PREFIX} ${HOST_BUILD} \
             --as=yasm                    \
             --enable-shared              \
             --disable-static             \
@@ -30,5 +39,5 @@ fi
             ${CPU_DETECT}                \
             --enable-experimental || { cat config.log; exit 1; }
 
-make -j${CPU_COUNT} V=1
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
