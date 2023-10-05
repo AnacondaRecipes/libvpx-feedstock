dfa4170040e53dddfd368c3fdc0e7825f2bab9a7
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2023-10-05 17:13:52 +0300

    [PKG-3023] libvpx 1.13.1 (#2)
    * libvpx 1.13.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,38 +6,34 @@ set -ex
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 if [[ $(uname) == MSYS* ]]; then
-  if [[ ${ARCH} == 32 ]]; then
-    HOST_BUILD="--host=i686-w64-mingw32 --build=i686-w64-mingw32"
-  else
     HOST_BUILD="--host=x86_64-w64-mingw32 --build=x86_64-w64-mingw32"
-  fi
-  PREFIX=${PREFIX}/Library/mingw-w64
+    PREFIX=${PREFIX}/Library/mingw-w64
 fi
 
 if [[ ${target_platform} == linux-* ]]; then
-  LDFLAGS="$LDFLAGS -pthread"
+    LDFLAGS="$LDFLAGS -pthread"
 fi
 
 CPU_DETECT=""
 if [[ ${target_platform} == osx-* ]]; then
-  CPU_DETECT="${CPU_DETECT} --disable-avx512 --disable-runtime-cpu-detect"
+    CPU_DETECT="${CPU_DETECT} --disable-avx512 --disable-runtime-cpu-detect"
 else
-  CPU_DETECT="${CPU_DETECT} --enable-runtime-cpu-detect"
+    CPU_DETECT="${CPU_DETECT} --enable-runtime-cpu-detect"
 fi
 
 ./configure --prefix=${PREFIX} ${HOST_BUILD} \
-            --as=yasm                    \
-            --enable-shared              \
-            --disable-static             \
-            --disable-install-docs       \
-            --disable-install-srcs       \
-            --enable-vp8                 \
-            --enable-postproc            \
-            --enable-vp9                 \
-            --enable-vp9-highbitdepth    \
-            --enable-pic                 \
-            ${CPU_DETECT}                \
-            --enable-experimental || { cat config.log; exit 1; }
+--as=yasm                    \
+--enable-shared              \
+--disable-static             \
+--disable-install-docs       \
+--disable-install-srcs       \
+--enable-vp8                 \
+--enable-postproc            \
+--enable-vp9                 \
+--enable-vp9-highbitdepth    \
+--enable-pic                 \
+${CPU_DETECT}                \
+--enable-experimental || { cat config.log; exit 1; }
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
